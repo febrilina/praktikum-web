b1a5fe2f65e2131948bc90a3578307a25c77b33f
febrilina <febrilina9@gmail.com>
2013-11-25 15:38:24 +0700

    laporan ke 5
    laporan ke 5

---
+++ 110533430618/Praktikum ke-5/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html> 
+<head> 
+	<title>Akses dan Manipulasi Data</title> 
+	<style type="text/css">
+	.even{
+	 background: #ddd;
+	}
+	</style>
+</head> 
+
+<body> 
+
+<?php
+ini_set('display_errors',1);
+
+//Meng-include file koneksi dan data handler
+require_once './koneksi.php';
+require_once './data_handler.php';
+
+//Konstanta nama tabel
+define('MHS','mahasiswa');
+
+//Mengambil fungsi data handler
+data_handler('?m=data');
+?>
+
+</body>
+</html>
---
+++ 110533430618/Praktikum ke-5/lat1.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html> 
+<head> 
+	<title>Tes Koneksi</title> 
+</head> 
+
+<body> 
+
+<?php
+require_once './koneksi.php';
+?>
+
+<h4>Koneksi Berhasil</h4>
+
+</body>
+</html>
